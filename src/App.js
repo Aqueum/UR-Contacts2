603e91153be91a70789a457a534945762125bfda
Aqueum <martin@aqueum.com>
2017-08-26 10:19:20 +0100

    Remove starter code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,3 @@
 import React, { Component } from 'react';
 
-class ContactList extends Component {
-  render() {
-    const people = this.props.contacts;
-
-    return (
-      <ol>
-        {people.map(person =>
-          <li key={person.name}>
-            {person.name}
-          </li>
-        )}
-      </ol>
-    );
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ContactList
-          contacts={[{ name: 'Me' }, { name: 'Myself' }, { name: 'I' }]}
-        />
-        <ContactList
-          contacts={[{ name: 'Does' }, { name: 'this' }, { name: 'work?' }]}
-        />
-      </div>
-    );
-  }
-}
-
 export default App;
