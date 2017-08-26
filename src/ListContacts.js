55721c5f7d2d17d99d08de5e988623fd3d68e031
Aqueum <martin@aqueum.com>
2017-08-26 12:41:43 +0100

    Set up PropTypes

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function ListContacts(props) {
   return (
@@ -30,5 +31,10 @@ function ListContacts(props) {
     </ol>
   );
 }
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired
+};
 
 export default ListContacts;
