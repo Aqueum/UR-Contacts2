09e989629a8ffd5153ed9b52ca607cff0ccfb6e8
Aqueum <martin@aqueum.com>
2017-08-26 14:05:37 +0100

    Make ListContacts a class again
    So it can hold state

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,35 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-function ListContacts(props) {
-  return (
-    <ol className="contact-list">
-      {props.contacts.map(contact =>
-        <li key={contact.id} className="contact-list-item">
-          <div
-            className="contact-avatar"
-            style={{
-              backgroundImage: `url(${contact.avatarURL})`
-            }}
-          />
-          <div className="contact-details">
-            <p>
-              {contact.name}
-            </p>
-            <p>
-              {contact.email}
-            </p>
-          </div>
-          <button
-            onClick={() => props.onDeleteContact(contact)}
-            className="contact-remove"
-          >
-            Remove
-          </button>
-        </li>
-      )}
-    </ol>
-  );
+class ListContacts extends Component {
+  render() {
+    return (
+      <ol className="contact-list">
+        {this.props.contacts.map(contact =>
+          <li key={contact.id} className="contact-list-item">
+            <div
+              className="contact-avatar"
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}
+            />
+            <div className="contact-details">
+              <p>
+                {contact.name}
+              </p>
+              <p>
+                {contact.email}
+              </p>
+            </div>
+            <button
+              onClick={() => this.props.onDeleteContact(contact)}
+              className="contact-remove"
+            >
+              Remove
+            </button>
+          </li>
+        )}
+      </ol>
+    );
+  }
 }
 
 ListContacts.propTypes = {
