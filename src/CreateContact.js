32f9910d75bd9fba36a8c9897711ed2741f99c05
Aqueum <martin@aqueum.com>
2017-08-28 13:09:57 +0100

    Add text inputs

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput';
 
 class CreateContact extends Component {
   render() {
@@ -8,6 +9,18 @@ class CreateContact extends Component {
         <Link className="close-create-contact" to="/">
           Close
         </Link>
+        <form className="create-contact-form">
+          <ImageInput
+            className="create-contact-avatar-input"
+            name="avatarURL"
+            maxHeight={64}
+          />
+          <div className="create-contact-details">
+            <input type="text" name="name" placeholder="Name" />
+            <input type="text" name="email" placeholder="Email" />
+            <button>Add contact</button>
+          </div>
+        </form>
       </div>
     );
   }
