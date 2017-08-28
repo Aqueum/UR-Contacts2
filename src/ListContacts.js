4934359255847121048063435286ca691b0025a1
Aqueum <martin@aqueum.com>
2017-08-28 12:09:57 +0100

    Fix import as Link is non default

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Link from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import escapeRegExp from 'escape-string-regexp';
 import sortBy from 'sort-by';
