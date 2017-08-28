34054011e28d40057bd0b58b4043a5e63111b668
Aqueum <martin@aqueum.com>
2017-08-28 13:30:58 +0100

    Add js serialisation

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ImageInput from './ImageInput';
+import serializeForm from 'form-serialize';
 
 class CreateContact extends Component {
   handleSubmit = e => {
     e.preventDefault();
+    const values = serializeForm(e.target, { hash: true });
+    if (this.props.onCreateContact) this.props.onCreateContact(values);
   };
   render() {
     return (
