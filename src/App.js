91a6d9f8574a9a23373aa509410f5da82ef6d336
Aqueum <martin@aqueum.com>
2017-08-28 19:36:13 +0100

    Create contact & redirect to list inc new contact

--- src/App.js
+++ src/App.js
@@ -22,6 +22,13 @@ class App extends Component {
     ContactsAPI.remove(contact);
   };
 
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([contact])
+      }));
+    });
+  }
   render() {
     return (
       <div className="app">
@@ -34,7 +41,16 @@ class App extends Component {
               onDeleteContact={this.removeContact}
             />}
         />
-        <Route path="/create" component={CreateContact} />
+        <Route
+          path="/create"
+          render={({ history }) =>
+            <CreateContact
+              onCreateContact={contact => {
+                this.createContact(contact);
+                history.push('/');
+              }}
+            />}
+        />
       </div>
     );
   }
