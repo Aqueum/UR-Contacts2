48ae9be5149994eeefcc04edf3041e2a606d82ff
Aqueum <martin@aqueum.com>
2017-08-28 13:20:21 +0100

    Prevent default serialization

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom';
 import ImageInput from './ImageInput';
 
 class CreateContact extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+  };
   render() {
     return (
       <div>
         <Link className="close-create-contact" to="/">
           Close
         </Link>
-        <form className="create-contact-form">
+        <form onSubmit={this.handleSubmit} className="create-contact-form">
           <ImageInput
             className="create-contact-avatar-input"
             name="avatarURL"
