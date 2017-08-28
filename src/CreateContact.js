134fc869b8d277f817ff4b6a69f8d99c12a367e1
Aqueum <martin@aqueum.com>
2017-08-28 12:50:15 +0100

    Add back button

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class CreateContact extends Component {
   render() {
-    return <div>Create Contact!</div>;
+    return (
+      <div>
+        <Link className="close-create-contact" to="/">
+          Close
+        </Link>
+      </div>
+    );
   }
 }
 
