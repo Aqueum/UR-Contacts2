0940538bb3b634613265769d8eb445d4f46c6322
Aqueum <martin@aqueum.com>
2017-08-28 12:04:54 +0100

    Change anchor to Link

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Link from 'react-router-dom';
 import PropTypes from 'prop-types';
 import escapeRegExp from 'escape-string-regexp';
 import sortBy from 'sort-by';
@@ -44,13 +45,9 @@ class ListContacts extends Component {
             value={query}
             onChange={event => this.updateQuery(event.target.value)}
           />
-          <a
-            href="#create"
-            onClick={this.props.onNavigate}
-            className="add-contact"
-          >
+          <Link to="/create" className="add-contact">
             Add Contact
-          </a>
+          </Link>
         </div>
 
         {showingContacts.length !== contacts.length &&
