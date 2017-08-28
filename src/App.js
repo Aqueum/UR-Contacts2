3ce31eac3685fcc860d8e0c9c21580226718e0f1
Aqueum <martin@aqueum.com>
2017-08-28 09:34:55 +0100

    Fix typo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
-import * as ContactsAPI from '/utils/ContactsAPI';
+import * as ContactsAPI from './utils/ContactsAPI';
 
 class App extends Component {
   state = {
