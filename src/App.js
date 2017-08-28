d1a698cd2deba80e00c503b44181e2cd8a560098
Aqueum <martin@aqueum.com>
2017-08-28 12:40:45 +0100

    Add routing from url

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import ListContacts from './ListContacts';
 import CreateContact from './CreateContact';
 import * as ContactsAPI from './utils/ContactsAPI';
@@ -25,15 +26,18 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        {this.state.screen === 'list' &&
-          <ListContacts
-            contacts={this.state.contacts}
-            onDeleteContact={this.removeContact}
-            onNavigate={() => {
-              this.setState({ screen: 'create' });
-            }}
-          />}
-        {this.state.screen === 'create' && <CreateContact />}
+        <Route
+          path="/"
+          render={() =>
+            <ListContacts
+              contacts={this.state.contacts}
+              onDeleteContact={this.removeContact}
+              onNavigate={() => {
+                this.setState({ screen: 'create' });
+              }}
+            />}
+        />
+        <Route path="/create" component={CreateContact} />
       </div>
     );
   }
